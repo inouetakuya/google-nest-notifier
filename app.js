f9132ad954af6d00284c9e6c612e3ff2649250cb
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-16 01:57:25 +0900

    ngrok で公開できるようにした

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const ngrok = require('ngrok')
+
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const server = app.listen(3000, async () => {
+  const port = server.address().port
+  const ngrokUrl = await ngrok.connect({ port: port })
+  console.log(`Forwarding: ${ngrokUrl} -> localhost:${port}`)
+})
