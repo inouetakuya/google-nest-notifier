adf03aee5eabb3298d9905adbf0a66a0107be2f0
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-16 19:09:39 +0900

    TypeScript をコンパイルできるようにした

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import { Request, Response } from 'express'
+
 const express = require('express')
 const ngrok = require('ngrok')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 
 const server = app.listen(3000, async () => {
   const port = server.address().port
