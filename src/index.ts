4b5859ee6d99bd035784521167f889685f53bed6
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-16 22:38:50 +0900

    production 環境でのみ ngrok を利用する

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,11 @@ app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 
 const server = app.listen(process.env.PORT || 3000, async () => {
   const port = server.address().port
-  const ngrokUrl = await ngrok.connect({ port: port })
-  console.log(`Forwarding: ${ngrokUrl} -> localhost:${port}`)
+
+  if (process.env.NODE_ENV === 'production') {
+    const ngrokUrl = await ngrok.connect({ port })
+    console.log(`Forwarding: ${ngrokUrl} -> localhost:${port}`)
+  } else {
+    console.log(`Development server: http://localhost:${port}`)
+  }
 })
