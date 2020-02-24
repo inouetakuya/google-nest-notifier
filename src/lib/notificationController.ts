3008c572724cb711152d303eeec96bb5902952cd
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-24 11:30:58 +0900

    + describe('when text is not set', () => { test('throws Error') }

--- src/lib/notificationController.ts
+++ src/lib/notificationController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express'
 const notificationController = {
   create: (request: Request, response: Response) => {
     const text = request.params.text
+    if (!text) throw new Error('text is required')
     response.status(201).json({ text })
   }
 }
--- test/lib/notificationController.spec.ts
+++ test/lib/notificationController.spec.ts
@@ -1,26 +1,45 @@
-import httpMocks from 'node-mocks-http'
+import httpMocks, { MockRequest, MockResponse } from 'node-mocks-http'
+import { Request, Response } from 'express'
 import notificationController from '~/lib/notificationController'
 
 describe('notificationController', () => {
   describe('create', () => {
-    let request
-    const response = httpMocks.createResponse()
+    let request: MockRequest<Request>
+    const response: MockResponse<Response> = httpMocks.createResponse()
 
-    beforeEach(() => {
-      request = httpMocks.createRequest({
-        method: 'POST',
-        url: '/notifications',
-        params: {
-          text: 'Hello world'
-        }
+    describe('when text is set', () => {
+      beforeEach(() => {
+        request = httpMocks.createRequest({
+          method: 'POST',
+          url: '/notifications',
+          params: {
+            text: 'Hello world'
+          }
+        })
+
+        notificationController.create(request, response)
       })
 
-      notificationController.create(request, response)
+      test('returns response successfully', () => {
+        expect(response.statusCode).toBe(201)
+        expect(response._getJSONData()).toEqual({ text: 'Hello world' })
+      })
     })
 
-    test('returns response successfully', () => {
-      expect(response.statusCode).toBe(201)
-      expect(response._getJSONData()).toEqual({ text: 'Hello world' })
+    describe('when text is not set', () => {
+      beforeEach(() => {
+        request = httpMocks.createRequest({
+          method: 'POST',
+          url: '/notifications',
+          params: {}
+        })
+      })
+
+      test('throws Error', () => {
+        expect(() => {
+          notificationController.create(request, response)
+        }).toThrowError('text is required')
+      })
     })
   })
 })
