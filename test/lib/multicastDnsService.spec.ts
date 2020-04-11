01730d7a71e2b91ab9e7f75fbd302b745eb6d6b8
INOUE Takuya <inouetakuya5@gmail.com>
2020-04-12 02:07:38 +0900

    fix: A function to advance timers was called but the timers API is not mocked with fake timers. Call `jest.useFakeTimers()` in this test or enable fake timers globally by setting `"timers": "fake"` in the configuration file. This warning is likely a result of a default configuration change in Jest 15.

--- test/lib/multicastDnsService.spec.ts
+++ test/lib/multicastDnsService.spec.ts
@@ -5,18 +5,20 @@ import { EventEmitter } from 'events'
 import * as multicastDnsService from '~/lib/multicastDnsService'
 import multicastDnsResponse from 'test/fixtures/multicastDnsResponse'
 
+jest.useFakeTimers()
+
 describe('multicastDnsService', () => {
+  // @ts-ignore TS7005: Variable 'browser' implicitly has an 'any' type.
+  let browser: any
+
+  beforeEach(() => {
+    browser = new EventEmitter()
+    browser.discover = jest.fn()
+    browser.stop = jest.fn()
+    mdns.createBrowser = () => browser
+  })
+
   describe('getMulticastDnsDataAll()', () => {
-    // @ts-ignore TS7005: Variable 'browser' implicitly has an 'any' type.
-    let browser: any
-
-    beforeEach(() => {
-      browser = new EventEmitter()
-      browser.discover = jest.fn()
-      browser.stop = jest.fn()
-      mdns.createBrowser = () => browser
-    })
-
     test('returns multicastDnsData[]', () => {
       const result = multicastDnsService
         .getMulticastDnsDataAll()
@@ -29,38 +31,25 @@ describe('multicastDnsService', () => {
       browser.emit('ready')
       browser.emit('update', multicastDnsResponse)
 
-      jest.runAllTimers()
+      jest.runOnlyPendingTimers()
 
       return result
     })
   })
 
   describe('queryMulticastDnsDataByDeviceName()', () => {
-    // @ts-ignore TS7005: Variable 'browser' implicitly has an 'any' type.
-    let browser: any
-
-    beforeEach(() => {
-      browser = new EventEmitter()
-      browser.discover = jest.fn()
-      browser.stop = jest.fn()
-      mdns.createBrowser = () => browser
-    })
-
     test('returns multicastDnsData[]', () => {
       const deviceName = 'Rachael'
-
       const result = multicastDnsService
         .queryMulticastDnsDataByDeviceName(deviceName)
         .then(dataArray => {
-          expect(browser.discover).toHaveBeenCalled()
-          expect(browser.stop).toHaveBeenCalled()
           expect(dataArray[0].deviceName).toBe(deviceName)
         })
 
       browser.emit('ready')
       browser.emit('update', multicastDnsResponse)
 
-      jest.runAllTimers()
+      jest.runOnlyPendingTimers()
 
       return result
     })
