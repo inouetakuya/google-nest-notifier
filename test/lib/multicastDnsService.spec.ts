1726c614dc46526006b15df6ac581e09fbf15d69
INOUE Takuya <inouetakuya5@gmail.com>
2020-04-11 19:53:56 +0900

    queryMulticastDnsDataByDeviceName() のテストをモックを使って書き直した

--- test/lib/multicastDnsService.spec.ts
+++ test/lib/multicastDnsService.spec.ts
@@ -1,13 +1,39 @@
-import multicastDnsService from '~/lib/multicastDnsService'
+// @ts-ignore TS7016: Could not find a declaration file for module 'mdns-js'
+import mdns from 'mdns-js'
 
-describe('apiGatewayService', () => {
+import { EventEmitter } from 'events'
+import multicastDnsService from '~/lib/multicastDnsService'
+import multicastDnsResponse from 'test/fixtures/multicastDnsResponse'
+
+describe('multicastDnsService', () => {
   describe('queryMulticastDnsDataByDeviceName()', () => {
-    test('returns multicastDnsData[]', async () => {
+    // @ts-ignore TS7005: Variable 'browser' implicitly has an 'any' type.
+    let browser: any
+
+    beforeEach(() => {
+      browser = new EventEmitter()
+      browser.discover = jest.fn()
+      browser.stop = jest.fn()
+      mdns.createBrowser = () => browser
+    })
+
+    test('returns multicastDnsData[]', () => {
       const deviceName = 'Rachael'
-      const dataArray = await multicastDnsService.queryMulticastDnsDataByDeviceName(
-        deviceName
-      )
-      expect(dataArray[0].deviceName).toBe('Rachael')
+
+      const result = multicastDnsService
+        .queryMulticastDnsDataByDeviceName(deviceName)
+        .then(dataArray => {
+          expect(browser.discover).toHaveBeenCalled()
+          expect(browser.stop).toHaveBeenCalled()
+          expect(dataArray[0].deviceName).toBe('Rachael')
+        })
+
+      browser.emit('ready')
+      browser.emit('update', multicastDnsResponse)
+
+      jest.runAllTimers()
+
+      return result
     })
   })
 })
