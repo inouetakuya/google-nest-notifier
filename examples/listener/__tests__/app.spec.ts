75af44aa739a56a925daa6c7bd31f1fc4b5f269d
INOUE Takuya <inouetakuya5@gmail.com>
2021-05-29 23:33:17 +0900

    test: test listener app with supertest

---
+++ examples/listener/__tests__/app.spec.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+import { app } from '../src/app'
+
+const mockedStatus = { volume: { muted: false } }
+const mockedNotify = jest.fn().mockResolvedValue(mockedStatus)
+const mockedGetIpAddress = jest.fn().mockResolvedValue('192.168.3.1')
+
+jest.mock('../../../packages/google-nest-notifier/src', () => {
+  return {
+    GoogleNestNotifier: jest.fn().mockImplementation(() => {
+      return { notify: mockedNotify, getIpAddress: mockedGetIpAddress }
+    }),
+  }
+})
+
+describe('app', () => {
+  describe('POST /notifications', () => {
+    describe('when all required params are set', () => {
+      it('returns response successfully', (done) => {
+        request(app)
+          .post('/notifications')
+          .set('Accept', 'application/json')
+          .send({
+            deviceName: 'Rachael',
+            text: 'Hello',
+            language: 'ja',
+          })
+          .expect('Content-Type', /json/)
+          .then((response) => {
+            expect(response.statusCode).toBe(201)
+            expect(response.body).toEqual({ status: mockedStatus })
+            done()
+          })
+      })
+    })
+  })
+})
