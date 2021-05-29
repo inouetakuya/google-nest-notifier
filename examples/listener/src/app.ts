1416b0d160af20cbbbfe103acbda8475bd3c5f1b
INOUE Takuya <inouetakuya5@gmail.com>
2021-05-30 01:43:16 +0900

    feat: handle errors

--- examples/listener/src/app.ts
+++ examples/listener/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { notificationsController } from './controllers/notificationsController'
 import { boomHandler } from './lib/boomHandler'
+import { errorHandler } from './lib/errorHandler'
 
 const app = express()
 
@@ -9,5 +10,6 @@ app.use(express.json())
 app.post('/notifications', notificationsController.create)
 
 app.use(boomHandler)
+app.use(errorHandler)
 
 export { app }
