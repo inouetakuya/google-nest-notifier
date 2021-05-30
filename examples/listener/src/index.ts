17b6d6f9ff41c790d4c31a161acda8e4952280f4
INOUE Takuya <inouetakuya5@gmail.com>
2021-05-30 23:34:28 +0900

    feat: fix listener url with API Gateway

--- examples/listener/src/index.ts
+++ examples/listener/src/index.ts
@@ -1,4 +1,5 @@
 import { app } from './app'
+import { apiGatewayService } from './lib/apiGatewayService'
 import { ngrokService } from './lib/ngrokService'
 
 const environment = process.env.NODE_ENV || 'development'
@@ -8,6 +9,8 @@ const server = app.listen(process.env.PORT || 3000, async () => {
   const port = server.address().port
 
   if (environment === 'production') {
+    console.log(process.env.USE_NGROK)
+
     if (process.env.USE_NGROK === '1') {
       const ngrokUrl = await ngrokService.connect({
         port,
@@ -17,6 +20,26 @@ const server = app.listen(process.env.PORT || 3000, async () => {
       console.log(
         [`Forwarding: ${ngrokUrl}`, `-> http://localhost:${port}`].join('\n')
       )
+
+      if (process.env.USE_API_GATEWAY === '1') {
+        const apiGatewayUrl = apiGatewayService.putIntegration({
+          region: process.env.API_GATEWAY_REGION,
+          restApiId: process.env.API_GATEWAY_REST_API_ID,
+          resourceId: process.env.API_GATEWAY_RESOURCE_ID,
+          httpMethod: process.env.API_GATEWAY_HTTP_METHOD,
+          url: ngrokUrl,
+          path: '/notifications',
+          profile: process.env.API_GATEWAY_PROFILE,
+        })
+
+        console.log(
+          [
+            `HttpProxy: ${apiGatewayUrl}`,
+            `-> ${ngrokUrl}/notifications`,
+            `-> http://localhost:${port}/notifications`,
+          ].join('\n')
+        )
+      }
     } else {
       console.log(`API Server running on http://localhost:${port}`)
     }
