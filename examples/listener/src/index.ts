28eb77e1fb82985088a0825c757cf842aaabde86
INOUE Takuya <inouetakuya5@gmail.com>
2021-05-30 20:24:29 +0900

    feat: output logs

--- examples/listener/src/index.ts
+++ examples/listener/src/index.ts
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv'
 import { app } from './app'
 
-dotenv.config()
-
-const index = app.listen(process.env.PORT || 3000, (): void => {
+const server = app.listen(process.env.PORT || 3000, (): void => {
   // @ts-ignore TS2531: Object is possibly 'null'.
-  const port = index.address().port
+  const port = server.address().port
   console.log(`API Server running on http://localhost:${port}`)
 })
