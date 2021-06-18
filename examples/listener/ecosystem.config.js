0482e15d800ca8424e4a539af20954b60457cda6
INOUE Takuya <inouetakuya5@gmail.com>
2021-06-19 00:34:56 +0900

    chore: update listener app name

--- examples/listener/ecosystem.config.js
+++ examples/listener/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'google-nest-notifier',
+      name: 'listener',
       script: 'dist/index.js',
 
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
