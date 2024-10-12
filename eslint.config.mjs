59275500beed2e0a20213a1820376d0f1bd9e68f
INOUE Takuya <inouetakuya5@gmail.com>
2024-10-12 17:40:02 +0900

    Only js.configs.recommended and ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,6 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import globals from 'globals'
-import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
 
 export default [
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ),
-  {
-    files: ['**/*.js', '**/*.ts'],
-    ignores: ['node_modules/**', 'dist/**'],
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-    },
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-      parser: tsParser,
-    },
-    rules: {
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-var-requires': 'off',
-    },
-  },
+  js.configs.recommended,
+  { ignores: ['**/node_modules/', '**/dist/'] },
 ]
