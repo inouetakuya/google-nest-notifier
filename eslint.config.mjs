cb23d8af955f901993d6946e2d1657949b058ac5
INOUE Takuya <inouetakuya5@gmail.com>
2024-10-12 17:53:09 +0900

    Set ignores with .gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
+import { includeIgnoreFile } from '@eslint/compat'
 import js from '@eslint/js'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
 
 export default [
   js.configs.recommended,
-  { ignores: ['**/node_modules/', '**/dist/'] },
+
+  // Ignore Files - ESLint - Pluggable JavaScript Linter
+  // https://eslint.org/docs/latest/use/configure/ignore
+  includeIgnoreFile(gitignorePath),
 ]
